c4b10e6514def3466357179b0b0ca633bcf61dcd
IamSankar97 <sankarsan.mohanty1234@gmail.com>
2022-11-29 17:54:37 +0800

    Updated function get_ctrl_points Added new_funtion bool_intersect, convert_mesh and main

--- generate_spacer_in_blender.py
+++ generate_spacer_in_blender.py
@@ -1,5 +1,5 @@
-import pydevd_pycharm
-pydevd_pycharm.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True)
+# import pydevd_pycharm
+# pydevd_pycharm.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True)
 import bpy
 from mathutils import Vector
 import numpy as np
@@ -11,15 +11,20 @@ def select_obj(objName, additive=False):
     bpy.context.scene.objects[objName].select_set(True)
 
 
-def get_ctrl_points(height_matrix: np.ndarray, grid_spacing: float):
+def get_ctrl_points(height_matrix: np.ndarray, grid_spacing: float, center=True):
     # Centring
-    x_y = int(height_matrix.shape[0] / 2)
+    if center:
+        end_right = int(height_matrix.shape[0] / 2)
+        end_left = -int(height_matrix.shape[0] / 2)
+    else:
+        end_right = height_matrix.shape[0]
+        end_left = 0
 
     ctrl_points = []
 
-    for i in range(-x_y, x_y):
+    for i in range(end_left, end_right):
         one_spline_ctrl_points = []
-        for j in range(-x_y, x_y):
+        for j in range(end_left, end_right):
             x, y = (i * grid_spacing) * 1e-6, (j * grid_spacing) * 1e-6  # To convert it to Meter from micrometer
             one_spline_ctrl_points.append(Vector(
                 (x, y, height_matrix[i][j] * 1e-6, 1))  # To convert it to Meter from micrometer
@@ -55,9 +60,22 @@ def generate_nurbs_surface(ctrl_points: list):
     bpy.ops.object.editmode_toggle()
 
 
+def bool_intersect(obj1, obj2):
+    bool_one = obj1.modifiers.new(type="BOOLEAN", name="bool 1")
+    bool_one.object = obj2
+    bool_one.operation = 'INTERSECT'
+    obj2.hide_set(True)
+
+
+def convert_mesh(obj: str):
+    select_obj(obj)
+    bpy.ops.object.convert(target='MESH')
+    bpy.ops.object.select_all(action='DESELECT')
+
+
 def main():
     my_realisation = np.genfromtxt(
-        "/home/mohanty/PycharmProjects/Digital-twin-for-hard-disk-spacer-ring-defect-detection/points_X50_dx184.85.csv",
+        "control_points/points_X50_dx184.85.csv",
         delimiter=',')
     dx = my_realisation[0][0]
     my_realisation = np.array(my_realisation[1:, :])
@@ -66,16 +84,12 @@ def main():
 
     generate_nurbs_surface(ctrl_points)
 
-    select_obj('NURBS_OBJ')
-    bpy.ops.object.convert(target='MESH')
-
     spacer_surf = bpy.data.objects['NURBS_OBJ']
     cylinder = bpy.data.objects['Cylinder']
 
-    bool_one = spacer_surf.modifiers.new(type="BOOLEAN", name="bool 1")
-    bool_one.object = cylinder
-    bool_one.operation = 'INTERSECT'
-    cylinder.hide_set(True)
+    convert_mesh('NURBS_OBJ')
+
+    bool_intersect(spacer_surf, cylinder)
 
 
 if __name__ == "__main__":
