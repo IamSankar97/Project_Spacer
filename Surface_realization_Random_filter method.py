921e29ff73fe1f43a9f89416761886c696b40cf5
IamSankar97 <sankarsan.mohanty1234@gmail.com>
2022-11-30 23:50:03 +0800

    Updated the code.

--- Surface_realization_Random_filter method.py
+++ Surface_realization_Random_filter method.py
@@ -1,35 +1,77 @@
 import slippy.surface as S
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 np.random.seed(0)
 
 
-def generate_random_filter_surface(filepath: str, length: float,  reduce_resolution_by_factor=1, grid_spacing=3.697,
-                                   no_of_grid_points=100):
+def fit_random_filter_surface(slippy_s: S, grid_spacing: float):
 
-    surf_height = pd.read_csv(filepath, delimiter=',')
-    surf_height.fillna(surf_height.mean(), inplace=True)  # Filling few empty cells with mean
-    slippy_s = S.assurface(surf_height)  # Creating Slippy surface object
-    slippy_s.grid_spacing = grid_spacing  # Data from original height1.csv -> "XY calibration"
-    slippy_s.get_psd()  # Calculating PSD
-    slippy_s.get_acf()  # Calculating acf
+    r""" Fits filter to generat random surface
+
+
+        Parameters
+        ----------
+        slippy_s: slippy.surface
+            A slippy surface; to be used to caculate the trage autocorrelation function of the surface independent of grid size
+        grid_spacing: float, optional (None)
+            The distance between surface points, must be set before the filter coefficients can be found
+     """
+
+    # grid_spacing
+
+    slippy_s.get_acf()  # Calculating acf independent of grid spacing
 
     # Generating Random spacer surface
-    random_surf = S.RandomFilterSurface(target_acf=slippy_s.acf, grid_spacing=slippy_s.grid_spacing)
-    random_surf.linear_transform(filter_shape=(35, 35), gtol=1e-5, symmetric=True, method='BFGS', max_it=1000)
+    lin_trans_surface = S.RandomFilterSurface(target_acf=slippy_s.acf, grid_spacing=grid_spacing)
+    lin_trans_surface.linear_transform(filter_shape=(35, 35), gtol=1e-5, symmetric=True, method='BFGS', max_it=1000)
+
+    return lin_trans_surface
+
+
+def generate_surface(lin_trans_surface, length: float, grid_spacing: float, grid_details: bool = False):
 
     no_of_grid_points = int(np.round(length / grid_spacing))
-    my_realisation = random_surf.discretise([no_of_grid_points, no_of_grid_points], periodic=True, create_new=True)
+    my_realisation = lin_trans_surface.discretise([no_of_grid_points, no_of_grid_points], periodic=True,
+                                                  create_new=True)
 
-    if reduce_resolution_by_factor != 1:
-        my_realisation.resample(random_surf.grid_spacing *
-                                reduce_resolution_by_factor)  # Scaling to increase grid spacing
-
-    print("Grid_Spacing: ", my_realisation.grid_spacing)
-    print("No_of_Ctrl_points: ", my_realisation.profile.shape[0])
+    if grid_details:
+        print("Grid_Spacing: ", my_realisation.grid_spacing)
+        print("No_of_grid_points: ", my_realisation.profile.shape[0])
 
     return my_realisation
+
+
+def compare_acf(lin_trans_surface, my_realisation, address: str):
+
+    target = lin_trans_surface.target_acf_array
+    my_realisation.get_acf()
+    actual = np.array(my_realisation.acf)
+    n, m = actual.shape
+    tn, tm = target.shape
+    actual_comparible = actual[n // 2:n // 2 + tn, m // 2:m // 2 + tm]
+    fig, axes = plt.subplots(1, 4, figsize=(16, 4))
+    axes[0].imshow(actual_comparible);
+    axes[0].set_title('Actual')
+    axes[1].imshow(target);
+    axes[1].set_title('Target')
+    axes[2].plot(actual_comparible[0, :], label='actual')
+    axes[2].plot(target[0, :], label='target')
+    axes[2].legend();
+    axes[2].set_title('Comparison along y direction')
+    axes[3].plot(actual_comparible[:, 0], label='actual')
+    axes[3].plot(target[:, 0], label='target')
+    axes[3].legend();
+    axes[3].set_title('Comparison along x direction')
+    plt.savefig(address)
+
+
+def get_properties(slippy_surface):
+    rou2 = slippy_surface.roughness(['sa', 'sq', 'ssk', 'sku'], no_flattening=True)
+    nam = ['Mean abs height: ', 'Root mean square height: ', 'Skew: ', 'Kurtosis: ']
+    for n, r in zip(nam, rou2):
+        print(n, r)
 
 
 def main():
@@ -40,11 +82,29 @@ def main():
 
     path = '/home/mohanty/PycharmProjects/Digital-twin-for-hard-disk-spacer-ring-defect-detection' \
            '/Spacer_Inspection/ｓ１/ｈｅｉｇｈｔ１_corrected.csv'
-    grid_spacing = 3.697
+    grid_spacing = 3.697  # Original grid_spacing
     spacer_outer_dia = (32.6 + 1.4) * 1000  # mm to micrometer *1000, adding buffer of 1.4 mm
-    reduce_resolution_by_factor = 10
+    reduce_resolution_by = 10
+    surf_height = pd.read_csv(path, delimiter=',')
+    surf_height.fillna(surf_height.mean(), inplace=True)  # Filling few empty cells with mean
+    slippy_s = S.assurface(surf_height)  # Creating Slippy surface object
+    slippy_s.grid_spacing = grid_spacing  # Data from original height1.csv -> "XY calibration"
 
-    my_realisation = generate_random_filter_surface(path, spacer_outer_dia, reduce_resolution_by_factor, grid_spacing)
+    desired_grid_spacing = grid_spacing * reduce_resolution_by
+    lin_trans_surface_original = fit_random_filter_surface(slippy_s, grid_spacing)
+    lin_trans_surface_realised = fit_random_filter_surface(slippy_s, desired_grid_spacing)
+
+    my_realisation = generate_surface(lin_trans_surface_realised, spacer_outer_dia, desired_grid_spacing, True)
+
+    compare_acf(lin_trans_surface_original, my_realisation, 'my_plot_X1_dx3.69to{}.png'
+                                                            .format(np.round(desired_grid_spacing),2))
+
+    print("\nProperties: Target\n")
+    get_properties(slippy_s)
+
+    print("\nProperties: actual\n")
+    get_properties(my_realisation)
+
     # Method to add grid spacing information
     realisation = pd.DataFrame(my_realisation.profile)
     realisation.loc[-1] = 0
@@ -52,7 +112,7 @@ def main():
     realisation.sort_index(inplace=True)
     realisation[0][0] = my_realisation.grid_spacing
 
-    np.savetxt('points_X{multi}_dx{dx}.csv'.format(multi=reduce_resolution_by_factor,
+    np.savetxt("points_X{multi}_dx{dx}.csv".format(multi=reduce_resolution_by,
                                                    dx=np.round(my_realisation.grid_spacing, 3)),
                np.array(realisation), delimiter=',')
 
