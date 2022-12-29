cf8e93c58aa988a65e831578b67750b408b52819
IamSankar97 <sankarsan.mohanty1234@gmail.com>
2022-12-29 23:33:24 +0800

    Created module for spacer and blender

--- generate_spacer.py
+++ generate_spacer.py
@@ -1,6 +1,13 @@
+# import pydevd_pycharm
+# pydevd_pycharm.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True)
+
+import sys
+import os
+sys.path.append(os.getcwd())
 from spacer import Spacer
 # from generate_spacer_in_blender import get_vertices
 import numpy as np
+from blender import Blender
 
 
 def main(address):
@@ -11,7 +18,19 @@ def main(address):
     spacer = Spacer(surface, grid_spacing)
     # spacer.get_point_co()
     spacer.generate_defect(13, 16, 0.5, 70, 40, 1)
-    print(spacer.point_coo)
+    blend = Blender(np.array(spacer.point_coo))
+    blend.set_scene_linear_unit('METERS')
+    blend.get_vertices()
+
+    csv_file = address.split('/')[-1].split('.')
+    name = csv_file[0] + '.' + csv_file[1]
+
+    blend.generate_polygon(name)
+    objects = blend.get_objs()
+    spacer_surf = objects[name]
+    cylinder = objects['Cylinder']
+    blend.bool_intersect(spacer_surf, cylinder)
+    blend.update_scene()
 
 
 if __name__ == "__main__":
