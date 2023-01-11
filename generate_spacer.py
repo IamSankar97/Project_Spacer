3a758ecdc0d243729927af3a3179be14f5f9fd5d
IamSankar97 <sankarsan.mohanty1234@gmail.com>
2023-01-11 21:24:58 +0800

    Updated generate surface function added preprocessing in yield statement

--- generate_spacer.py
+++ generate_spacer.py
@@ -14,54 +14,56 @@ import time
 
 def get_sample_surface(folder):
     while True:
+        count = 0
         for filename in os.listdir(folder):
             if filename.endswith('.pkl'):
+                count += 1
                 with open(os.path.join(folder, filename), 'rb') as f:
                     my_realisation = pickle.load(f) * 1e-6
                     grid_spacing = my_realisation[0][0]
-                    surface = np.array(my_realisation[1:, :])
-                    yield surface, grid_spacing
+                    sample_surface = np.array(my_realisation[1:, :])
+
+                    spacer = Spacer(sample_surface, grid_spacing)
+                    ro, r1, theta0, defect_length, defect_type = \
+                        np.round(np.random.uniform(12.5, 13.2), 2), \
+                        np.round(np.random.uniform(13.8, 16), 2), \
+                        np.random.randint(1, 85), \
+                        np.round(np.random.uniform(2, 10), 2), \
+                        count % 2
+                    spacer.randomize_defect(ro, r1, theta0, 70, 40, defect_length, defect_type)
+
+                    yield spacer, ro, r1, theta0, defect_length, defect_type
 
 
 def main(addr_topology: str, addr_img_save: str):
     count = 0
-    for sample_surface, grid_spacing in get_sample_surface(addr_topology):
-        start = time.time()
+    global spacer_surf, blend, start
+    start = time.time()
+    for spacer, ro, r1, theta0, defect_length, defect_type in get_sample_surface(addr_topology):
         count += 1
-        spacer = Spacer(sample_surface, grid_spacing)
-        ro, r1, theta0, defect_length, defect_type = \
-            np.round(np.random.uniform(12.5, 13.2), 2), \
-            np.round(np.random.uniform(13.8, 16), 2), \
-            np.random.randint(1, 85), \
-            np.round(np.random.uniform(2, 10), 2), \
-            count % 2
-        spacer.randomize_defect(ro, r1, theta0, 70, 40, defect_length, defect_type)
-
-        global spacer_surf, blend
         if count == 1:
             blend = Blender(np.array(spacer.point_coo))
             objects = blend.get_objs()
             blend.set_scene_linear_unit('METERS')
             blend.generate_polygon('my_mesh')
+
+            time_poly = time.time() - start
             spacer_surf = objects['my_mesh']
             blend.assign_material(spacer_surf)
-            blend.update_mat({'Roughness': 0, 'Metallic': 1})
-            result_image = blend.render(file_path=addr_img_save, save=True, engine='CYCLES')
-            end = time.time()
-            print("Total time elapsed_ {}_st generation:".format(count), end - start)
-            result_image.save('spacer_render/image_{cout}_{r0}r0_{r1}r1_{th}th0_{ln}ln_{ty}ty.png'
-                              .format(cout=count, r0=ro, r1=r1, th=theta0, ln=defect_length, ty=defect_type))
 
         else:
             blend.update_mesh_back_ground(np.array(spacer.point_coo), spacer_surf)
-            #   blend.assign_material(spacer_surf)
-            blend.update_mat({'Roughness': 0, 'Metallic': 1})
-            result_image = blend.render(file_path=addr_img_save, save=True, engine='CYCLES')
-            end = time.time()
-            print("Total time elapsed_{}_st generation:".format(count), end - start)
-            # result_image.show()
-            result_image.save('spacer_render/image_{cout}_{r0}r0_{r1}r1_{th}th0_{ln}ln_{ty}ty.png'
-                              .format(cout=count, r0=ro, r1=r1, th=theta0, ln=defect_length, ty=defect_type))
+            time_poly = time.time() - start
+
+        start = time.time()
+        blend.update_mat({'Roughness': 0, 'Metallic': 1})
+        result_image = blend.render(file_path=addr_img_save, save=True, engine='CYCLES')
+        time_render = time.time() - start
+        print("time elapsed_ {}_st generation for poly_mesh: {} and for rendering: {}:"
+              .format(count, time_poly, time_render))
+        result_image.save('spacer_render/image_{cout}_{r0}r0_{r1}r1_{th}th0_{ln}ln_{ty}ty.png'
+                          .format(cout=count, r0=ro, r1=r1, th=theta0, ln=defect_length, ty=defect_type))
+        start = time.time()
 
 
 if __name__ == "__main__":
